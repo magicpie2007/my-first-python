c0d0b3b8e7f04f181313a7d08b2bd8b0e847df58
HiroyukiA.Shoji <Hiroyuki.Shoji@sony.com>
2017-07-24 20:10:06 +0900

    Add actual implements

--- practice/extractdata.py
+++ practice/extractdata.py
@@ -5,10 +5,15 @@ import re
 
 
 def main(args):
-   csv = open(args[-1], 'r')
-   for arg in args[0:-1]:
-       pos = parsearg(arg)
-       extractdata(pos)
+    csvfile = open(args[-1], 'r')
+    extracttype = args[0]
+    for arg in args[1:-1]:
+        pos = parsearg(arg)
+        data = extract(csvfile, pos, extracttype)
+        print(data)
+        csvfile.seek(0)
+
+    csvfile.close()
 
 
 def parsearg(arg):
@@ -17,18 +22,63 @@ def parsearg(arg):
     regex = re.compile(r'(\d+)([cr])((\d+)([cr]))?')
     mo = regex.search(arg)
     if mo.group(2) == 'c':
-        columnnum = mo.group(1)
+        columnnum = int(mo.group(1))
         if mo.group(3) is not None and mo.group(5) == 'r':
-            rownum = mo.group(4)
+            rownum = int(mo.group(4))
     elif mo.group(2) == 'r':
-        rownum = mo.group(1)
+        rownum = int(mo.group(1))
         if mo.group(3) is not None and mo.group(5) == 'c':
-            columnnum = mo.group(4)
+            columnnum = int(mo.group(4))
     return {'column': columnnum, 'row': rownum}
 
 
-def extractdata(pos):
-    pass
+def extract(csvfile, pos, extracttype):
+    if pos['column'] is not None and pos['row'] is not None:
+        raw_data = extract_cell(csvfile, pos['column'], pos['row'])
+    elif pos['column'] is not None:
+        raw_data = extract_column(csvfile, pos['column'])
+    elif pos['row'] is not None:
+        raw_data = extract_row(csvfile, pos['row'])
+    else:
+        print("Error: Invalid argument")
+        sys.exit(1)
+
+    data = []
+    if extracttype == 'str':
+        data = raw_data
+    elif extracttype == 'int':
+        for item in raw_data:
+            try:
+                data.append(int(item))
+            except ValueError:
+                data.append(item)
+    elif extracttype == 'float':
+        for item in raw_data:
+            try:
+                data.append(float(item))
+            except ValueError:
+                data.append(item)
+
+    return data
+
+
+def extract_cell(csvfile, columnnum, rownum):
+    rawdata_column = []
+    line_rawdata = csvfile.readlines()[rownum - 1].split(',')
+    rawdata_column.append(line_rawdata[columnnum - 1])
+    return rawdata_column
+
+
+def extract_column(csvfile, columnnum):
+    rawdata_column = []
+    for line in csvfile:
+        line_rawdata = line.split(',')
+        rawdata_column.append(line_rawdata[columnnum - 1])
+    return rawdata_column
+
+
+def extract_row(csvfile, rownum):
+    return csvfile.readlines()[rownum - 1].split(',')
 
 
 if __name__ == '__main__':
