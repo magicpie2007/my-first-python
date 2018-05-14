f550b9bb9e3e5474e2396d653d4b11dbcdb50f67
HiroyukiA.Shoji <Hiroyuki.Shoji@sony.com>
2018-05-14 11:46:24 +0900

    Add summing commit counts of each column
    Add summing commit counts of each column and make the sum data added to
    last row of csv.
    And some modification for graph style or configuration are added.

--- practice/repocc.py
+++ practice/repocc.py
@@ -12,7 +12,8 @@ import numpy
 
 def main(args):
     branch, since, until, interval, regex, inverse_regex = parse_args_for_cc(args)
-    output_file_name = create_file_name(branch, since, until, interval)
+    output_file_name = create_file_name(branch, since, until, interval,
+                                        regex, inverse_regex)
     counting_commit(branch, since, until, interval,
                     regex, inverse_regex, output_file_name)
     drawing_graph(output_file_name)
@@ -33,10 +34,12 @@ def drawing_graph(output_file_name):
     csv_file.seek(0)
 
     # Sort the data and get its index
-    sorted_index = numpy.argsort(numpy.array(data[1:]))
+    sorted_index = numpy.argsort(numpy.array(data[1:-1]))
 
     # Define graph type from pygal
-    graph = pygal.HorizontalStackedBar()
+    graph_detail = pygal.HorizontalStackedBar(title='Top 20 Repositories (Commit)')
+    graph_summary = pygal.HorizontalStackedBar(title='Top 20 Repositories (Commit)',
+                                               show_legend=False)
 
     # Set x labels
     x_labels_data = []
@@ -45,7 +48,8 @@ def drawing_graph(output_file_name):
         cell = extractdata.extract(csv_file, pos, 'str')
         csv_file.seek(0)
         x_labels_data.append(cell[0])
-    graph.x_labels = tuple(x_labels_data)
+    graph_detail.x_labels = tuple(x_labels_data)
+    graph_summary.x_labels = tuple(x_labels_data)
 
     # Add data to graph
     for i in range(1, column_size - 1):
@@ -55,9 +59,20 @@ def drawing_graph(output_file_name):
             cell = extractdata.extract(csv_file, pos, 'int')
             csv_file.seek(0)
             graph_data.append(cell[0])
-        graph.add(header[i], graph_data)
+        graph_detail.add(header[i], graph_data)
+    graph_summary_data = []
 
-    graph.render_to_file(output_file_name + '.svg')
+    for i in sorted_index[-20:]:
+        pos = {'column': column_size, 'row': i + 1 + 1}
+        cell = extractdata.extract(csv_file, pos, 'int')
+        csv_file.seek(0)
+        graph_summary_data.append(cell[0])
+    graph_summary.add('', graph_summary_data)
+
+    graph_detail.render_to_file(output_file_name + '.svg')
+    graph_detail.render_to_png(output_file_name + '.png')
+    graph_summary.render_to_file(output_file_name + '_summary.svg')
+    graph_summary.render_to_png(output_file_name + '_summary.png')
 
 
 def counting_commit(branch, since, until, interval,
@@ -66,18 +81,20 @@ def counting_commit(branch, since, until, interval,
     output_file = open(output_file_name + ".csv", 'w')
 
     # Write header
-    write_header_for_cc(since, until, interval, output_file)
+    num_column = write_header_for_cc(since, until, interval, output_file)
 
     # Execute counting commit
     executing_counting_commit(branch, since, until, interval,
-                              regex, inverse_regex, output_file)
+                              regex, inverse_regex,
+                              num_column, output_file)
 
     # Close
     output_file.close()
 
 
 def executing_counting_commit(branch, since, until, interval,
-                              regex, inverse_regex, output_file):
+                              regex, inverse_regex,
+                              num_column, output_file):
 
     # Create repo command
     repo_cmd = "repo forall -p -v "
@@ -92,11 +109,10 @@ def executing_counting_commit(branch, since, until, interval,
                       " " + interval
     cmd = (repo_cmd + repo_forall_cmd).split()
 
-    print(cmd)
-
     exe = subprocess.Popen(cmd, stdout=subprocess.PIPE, encoding='utf-8')
 
     project = None
+    sum_columns = numpy.zeros(num_column - 1, numpy.int32)
     while True:
         polling = exe.poll()
         if polling is not None:
@@ -105,6 +121,9 @@ def executing_counting_commit(branch, since, until, interval,
             if line.startswith('project '):
                 project = line[len('project '):-1]
             elif line.startswith(','):
+                # Sum columns
+                line_array = numpy.array(list(map(int, line[1:].split(','))), numpy.int32)
+                sum_columns += line_array
                 output_line = project + line
                 output_file.write(output_line)
             elif line == '' or line == '\n':
@@ -112,6 +131,10 @@ def executing_counting_commit(branch, since, until, interval,
                 continue
             else:
                 output_file.write(line)
+
+    # Write sum of each column
+    output_file.write('Sum,')
+    output_file.write(','.join(list(map(str, sum_columns.tolist()))))
     exe.stdout.close()
 
 
@@ -138,21 +161,33 @@ def write_header_for_cc(since, until, interval, output_file):
 
     date = date_since + delta
     output_file.write("Project")
+    num_column = 1
     while date <= date_until:
         date_adj = date - datetime.timedelta(days=1)
         output_file.write(",%s" % date_adj.strftime('%Y-%m-%d'))
         date = date + delta
+        num_column += 1
 
     if date > date_until:
         output_file.write(",%s" % date_until.strftime('%Y-%m-%d'))
+        num_column += 1
 
     output_file.write(",Sum\n")
+    num_column += 1
+
+    return num_column
 
 
-def create_file_name(branch, since, until, interval):
+def create_file_name(branch, since, until, interval, regex, inverse_regex):
     branch_name = branch.replace('/', '_')
-    return "Commit_Counts_" + branch_name + \
-           '_' + since + '-' + until + '_' + interval
+    file_name = "Commit_Counts_" + branch_name + \
+                '_' + since + '-' + until + '_' + interval
+    if regex is not None:
+        file_name += '_' + regex.replace('/', '_')
+    if inverse_regex is not None:
+        file_name += '_wo_' + inverse_regex.replace('/', '_')
+
+    return file_name
 
 
 def parse_args_for_cc(args):
